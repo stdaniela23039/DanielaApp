b2f49effe7cae9dd940f19cfacef58a95b9b277f
bstp <bstautas@iemsantateresita.edu.co>
2022-08-18 12:33:41 -0500

    Initial commit

---
+++ app/src/main/java/com/stasoft/danielaapp/inicioActivity.kt
@@ -0,0 +1,11 @@
+package com.stasoft.danielaapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class inicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
